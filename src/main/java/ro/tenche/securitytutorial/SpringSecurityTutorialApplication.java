f5cc3c9e857976a192b2c2b2c27c474d733613a8
Alexandru Tenche <alex.tenche@gmail.com>
2021-10-31 09:57:14 +0200

    reformat

--- src/main/java/ro/tenche/securitytutorial/SpringSecurityTutorialApplication.java
+++ src/main/java/ro/tenche/securitytutorial/SpringSecurityTutorialApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityTutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityTutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTutorialApplication.class, args);
+    }
 
 }
--- src/main/java/ro/tenche/securitytutorial/student/StudentController.java
+++ src/main/java/ro/tenche/securitytutorial/student/StudentController.java
@@ -19,7 +19,7 @@ public class StudentController {
     );
 
     @GetMapping(path = "{studentId}")
-    public Student getStudent(@PathVariable("studentId") Integer studentId){
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
         return STUDENTS.stream()
                 .filter(student -> studentId.equals(student.getStudentId()))
                 .findFirst()
