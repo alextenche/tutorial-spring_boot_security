2e81f3662ab3d2c56c7fb2e839ce19dd582a6ff2
Alexandru Tenche <alex.tenche@gmail.com>
2021-11-06 16:23:03 +0200

    using application user service

--- src/main/java/ro/tenche/securitytutorial/security/ApplicationSecurityConfig.java
+++ src/main/java/ro/tenche/securitytutorial/security/ApplicationSecurityConfig.java
@@ -1,18 +1,17 @@
 package ro.tenche.securitytutorial.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import ro.tenche.securitytutorial.auth.ApplicationUserService;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,10 +23,13 @@ import static ro.tenche.securitytutorial.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
+                                     ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -67,34 +69,46 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutSuccessUrl("/login");
     }
 
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails studentUser = User.builder()
-                .username("student")
-                .password(passwordEncoder.encode("pass"))
-//                .roles(STUDENT.name())
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
 
-        UserDetails adminTraineeUser = User.builder()
-                .username("admin_trainee")
-                .password(passwordEncoder.encode("pass"))
-//                .roles(ADMIN_TRAINEE.name())
-                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
-                .build();
-
-        UserDetails adminUser = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("pass"))
-//                .roles(ADMIN.name())
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-
-        return new InMemoryUserDetailsManager(
-                studentUser,
-                adminTraineeUser,
-                adminUser);
+        return provider;
     }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails studentUser = User.builder()
+//                .username("student")
+//                .password(passwordEncoder.encode("pass"))
+////                .roles(STUDENT.name())
+//                .authorities(STUDENT.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails adminTraineeUser = User.builder()
+//                .username("admin_trainee")
+//                .password(passwordEncoder.encode("pass"))
+////                .roles(ADMIN_TRAINEE.name())
+//                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails adminUser = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder.encode("pass"))
+////                .roles(ADMIN.name())
+//                .authorities(ADMIN.getGrantedAuthorities())
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(
+//                studentUser,
+//                adminTraineeUser,
+//                adminUser);
+//    }
 }
